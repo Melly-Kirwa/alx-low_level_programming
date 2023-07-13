c30ee3fdfebf04c7ef446057cb3a9ae0e5d54443
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-13 06:12:13 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+*
+*Main - Entry point
+*
+*Return: Always zero(Success)
+*
+*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
