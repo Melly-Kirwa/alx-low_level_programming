cdf99abc637e0efdedf7a3005d7688f1746bf781
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-14 08:28:17 -0700

    numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0-9
+ *
+ * return - always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
