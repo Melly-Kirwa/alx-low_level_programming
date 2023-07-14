335f4d48721bbaf94553fe6af61347ed9d470ceb
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-14 10:19:46 -0700

    2 digit combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of 2 digit numbers without repetition
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = 1; y <= 9; y++)
+		{
+			putchar('0' + x);
+			putchar('0' + y);
+
+			if (x < 8 || y < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
