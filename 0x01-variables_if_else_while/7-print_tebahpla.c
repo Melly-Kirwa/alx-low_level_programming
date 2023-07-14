e682ef871c108a922c428a3242435ce01eaafd7e
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-14 09:09:14 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for(i='z'; i>='a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
