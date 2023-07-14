cbc31f3b0a5a2fab919e290be38b3a53c2c4cdb8
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-14 03:35:13 -0700

    correction 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function goes there */
+/**
+ * more headers goes there
+ *
+ * betty style doc for function goes there
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
