339322e6ebb69ba07ba1c3210cd49e5f6668d2c3
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-18 03:37:28 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether it is letter (uppercase or lowercase)
+ * @c: charater to check
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+	_putchar('\n');
+}
