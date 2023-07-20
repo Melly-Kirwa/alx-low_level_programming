7df5f499e8a436ae2c2b12134d87f0a94da6792e
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-20 14:09:01 -0700

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square on the terminal
+ * @size: character to check
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
