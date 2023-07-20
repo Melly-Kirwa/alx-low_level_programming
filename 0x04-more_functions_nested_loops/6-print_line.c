924acf17e7faac0cd2dd1829dc7778b214474d14
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-20 12:18:55 -0700

    continuous line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print _ n times
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c <= n; c++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
