6dae61f19ac0bbb4e8a9418ab39b977ab54df16c
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-20 10:34:08 -0700

    numbers 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 1-14 ten times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j = 0;
+
+	while (j < 14)
+	{
+		i = 0;
+		while( i <= 14)
+		{
+			if (i < 10)
+			{
+				_putchar('0' + i);
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			i++;
+		}
+		j++;
+		_putchar('\n');
+	}
+}
