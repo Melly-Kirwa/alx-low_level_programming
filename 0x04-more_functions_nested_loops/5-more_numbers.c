ae4ef33fd3e80d2462f152f59ba15e02717e8ddb
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-20 10:48:55 -0700

    Correction 3

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,16 @@
 
 void more_numbers(void)
 {
-	int i;
-	int j = 0;
+	int i, j;
 
-	while (j < 14)
+	for (i = 0; i <= 9; i++)
 	{
-		i = 0;
-		while( i <= 14)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
-			}
-			_putchar((i % 10) + '0');
-			i++;
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
-		j++;
 		_putchar('\n');
 	}
 }
