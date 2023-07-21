278f7c50e72a410dc82db5fb0392810f91af9cdf
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-21 01:26:20 -0700

    corrections again

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,13 +14,15 @@ int main(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if ((x % 3) == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if ((x % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (((x % 3) == 0) && ((x % 5) == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else
-			printf("%d ", x);
+			printf("%d", x);
+		if (x < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
