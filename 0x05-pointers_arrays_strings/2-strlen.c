c19d6877c2e3c27627ad354a6bd32d4d923c1e8b
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-24 04:02:44 -0700

    count string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - evaluates the length of a string
+ * @s: string to check
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	return count;
+}
