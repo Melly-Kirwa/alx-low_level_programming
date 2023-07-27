ae299fe9ec43a74ff711f24d515b01312bcd17b9
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-27 07:37:24 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *rot13 - encodes a string using rot13 encoding scheme
+ * @str: input string
+ * Return: pointer to the encoded string
+ */
+
+char *rot13(char *str)
+{
+	int j;
+	int i = 0;
+	char alp[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+	char r_alp[] ={"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alp[j])
+			{
+				str[i] = r_alp[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
