f0deca5cee42693e7f92b1df60d681de1b484955
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-27 05:06:58 -0700

    trial 3

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,10 +24,10 @@ char *cap_string(char *str)
 			{
 				if (str[i + 1] >= 97 && str[i + 1] <= 122)
 				{
-					str[i + 1] = str[i + 1] - 32;
+					str[i + 1] -= 32;
 				}
 			}
-       		}
+		}
 		i++;
 	}
 	return (str);
