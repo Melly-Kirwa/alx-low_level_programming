fbe84cec8d68db1e41efc095f6a5e4ddee8175e1
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-27 06:09:46 -0700

    1337 encoder

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - takes a string of input and performs 1337 encoding
+ * @str: input string
+ * Return: pointer to the ecoded string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	int rep_upp[] = {65, 69, 79, 84, 76};
+	int rep_low[] = {97, 101, 111, 116, 108};
+	int rep_int[] = {52, 51, 48, 55, 49};
+
+	i = 0;
+	while ( str[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if ((str[i] = rep_upp[j]) || (str[i] == rep_low[j]))
+			{
+				str[i] = rep_int[j];
+			}
+		}
+		i++;
+	}
+	return (str);
+}
