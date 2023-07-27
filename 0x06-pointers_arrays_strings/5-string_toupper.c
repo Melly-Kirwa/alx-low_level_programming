89b556ce7506cbbb1bca66fe9c13d432d21cc19e
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-27 02:45:29 -0700

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - converts from lowercase to uppercase
+ * @str: string to convert
+ * Return: pointer to converted string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
