7e932c1efc0fdd801ff34b1a93fb5a842b68dac9
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-27 01:13:49 -0700

    correction 1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' || *s2 != '\0')
+	int i = 0;
+
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (*s1 != *s2)
+		if (s1[i] != s2[i])
 		{
-			return (*s1 - *s2);
+			return (s1[i] - s2[i]);
 		}
 		return (0);
+		i++;
 	}
 	return (0);
 }
