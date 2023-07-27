728a57322bcd2837817d15cf706db92eec9ac345
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-27 04:50:49 -0700

    correction on horizontal tab

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,24 +9,23 @@
 char *cap_string(char *str)
 {
 	int i, j;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int sep_words[] = {32, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		for (j = 0; j < 13; j++)
+		for (j = 0; j < 12; j++)
 		{
-			if (str[i] == sep_words[1])
+			if (str[i] == 9)
 			{
-				str[i] = sep_words[0];
+				str[i] = 32;
 			}
-			else if (str[i] == sep_words[j])
+			if (str[i] == sep_words[j])
 			{
 				if (str[i + 1] >= 97 && str[i + 1] <= 122)
 				{
 					str[i + 1] = str[i + 1] - 32;
 				}
-				break;
 			}
        		}
 		i++;
