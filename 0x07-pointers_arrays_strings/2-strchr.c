c752e37b63a67908383c85030cb1071a329b6773
Melly-Kirwa <corneliusmelly@gmail.com>
2023-07-31 06:11:37 -0700

    firdt occurrence

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character on the string
+ * @s: string to check
+ * @c: character to check
+ * Return: pointer to the first occurence of the character checked
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
