a22aceffa6adc6a9b1141df1295919c22d251ed6
Melly-Kirwa <corneliusmelly@gmail.com>
2023-08-04 12:35:35 -0700

    updated readme,header file and created first program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed
+ * @argv: array of arguments passed
+ * Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return(0);
+}
