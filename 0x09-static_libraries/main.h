38ff164bb8802a5dcd6f4f6f2a9dbcd33ab027f2
Melly-Kirwa <corneliusmelly@gmail.com>
2023-08-04 11:08:43 -0700

    trial 2

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int n);
+int _abs(int i);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
@@ -18,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
